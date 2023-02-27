7303b27e8509debfe3dedb86f5592fca33422196
HackTheOxidation <tomas.hagenau@protonmail.ch>
2023-02-27 21:52:59 +0100

    Move python to root

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from setuptools import Extension, setup
 
 libcprdetector = Extension(name="os2ds_rules.cpr_detector",
                            language="c++",
-                           include_dirs=["../include/"],
-                           sources=["src/os2ds_rules/ffi.cpp", "../lib/cpr-detector.cpp"],
+                           include_dirs=["include/"],
+                           sources=["src/os2ds_rules/ffi.cpp", "lib/cpr-detector.cpp"],
                            extra_compile_args=["-O3", "-std=c++20"])
 
 setup(ext_modules=[libcprdetector])
