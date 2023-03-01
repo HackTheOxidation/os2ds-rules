3afd3a058e627b0d63e1a07e3b5a8244b1ffc61a
HackTheOxidation <tomas.hagenau@protonmail.ch>
2023-03-01 09:21:30 +0100

    Changed setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,23 @@
 from setuptools import Extension, setup
 
-libcprdetector = Extension(name="os2ds_rules.cpr_detector",
+CXX_FLAGS = (
+    "-O3",
+    "-std=c++20",
+    )
+
+CPR_SOURCES = (
+    "src/os2ds_rules/cpr.cpp",
+    "lib/cpr-detector.cpp",
+    )
+
+cpr_detector = Extension(name="os2ds_rules.cpr_detector",
                            language="c++",
                            include_dirs=["include/"],
-                           sources=["src/os2ds_rules/ffi.cpp", "lib/cpr-detector.cpp"],
-                           extra_compile_args=["-O3", "-std=c++20"])
+                           sources=[*CPR_SOURCES],
+                           extra_compile_args=[*CXX_FLAGS])
 
-setup(ext_modules=[libcprdetector])
+setup(
+    ext_modules=[
+        cpr_detector,
+      ]
+    )
