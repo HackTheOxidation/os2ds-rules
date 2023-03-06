07c15d7f9a6f035fb92d19333fac89592221eaec
HackTheOxidation <tomas.hagenau@protonmail.ch>
2023-03-06 18:36:36 +0100

    Load wordlists at compile-time.

--- include/data_structures.hpp
+++ include/data_structures.hpp
@@ -1,9 +1,36 @@
 #ifndef DATA_STRUCTURES_HPP
 #define DATA_STRUCTURES_HPP
 
+#include <array>
+#include <memory>
+#include <string_view>
+
 namespace OS2DSRules {
 
 namespace DataStructures {
+
+class Names {
+    private:
+    static constexpr std::string_view male_firstnames_[] = {
+        #include "datasets/male_firstnames.txt"
+    };
+    static constexpr std::string_view female_firstnames_[] = {
+        #include "datasets/female_firstnames.txt"
+    };
+    static constexpr std::string_view lastnames_[] = {
+        #include "datasets/lastnames.txt"
+    };
+    static constexpr std::string_view addresses_[] = {
+        #include "datasets/da_addresses.txt"
+    };
+
+    public:
+    static constexpr auto male_firstnames = std::to_array(std::move(male_firstnames_));
+    static constexpr auto female_firstnames = std::to_array(std::move(female_firstnames_));
+    static constexpr auto lastnames = std::to_array(std::move(lastnames_));
+    static constexpr auto addresses = std::to_array(std::move(addresses_));
+    Names() = delete;
+};
 
 class Trie;
 
