ec9dc770637f32573cc0604bb870d5a1b6627166
HackTheOxidation <tomas.hagenau@protonmail.ch>
2023-03-19 17:06:34 +0100

    Prefer Trie as a concept rather than a class.

--- include/data_structures.hpp
+++ include/data_structures.hpp
@@ -2,20 +2,38 @@
 #define DATA_STRUCTURES_HPP
 
 #include <array>
+#include <concepts>
 #include <cstddef>
+#include <string>
 #include <string_view>
+#include <type_traits>
 
 namespace OS2DSRules {
 
 namespace DataStructures {
 
-  class Trie {
+  template<typename T>
+  concept Trie = requires(T trie, std::string str, const char* cp, std::string_view strv) {
+    { trie.size() } -> std::same_as<size_t>;
+    { trie.contains(strv) } -> std::same_as<bool>;
+    { trie.contains(str) } -> std::same_as<bool>;
+    { trie.contains(cp) } -> std::same_as<bool>;
+  };
+
+  template<typename T>
+  concept MutableTrie = Trie<T> && requires(T trie, std::string str) {
+    trie.insert(str);
+  };
+
+  class ReadOnlyTrie {
     public:
-    Trie() noexcept;
-    size_t size() const noexcept;
+    constexpr ReadOnlyTrie() noexcept;
+    constexpr size_t size() const noexcept {
+      return size_;
+    };
 
     private:
-    size_t size_;
+    const size_t size_;
   };
 
 }; // DataStructures
