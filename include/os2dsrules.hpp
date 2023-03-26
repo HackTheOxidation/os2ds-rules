3be1d812926e168eb5b877a901a88476163b5298
HackTheOxidation <tomas.hagenau@protonmail.ch>
2023-03-26 12:47:07 +0200

    Add some convenient methods to MatchResult.

--- include/os2dsrules.hpp
+++ include/os2dsrules.hpp
@@ -18,11 +18,30 @@ enum class Sensitivity {
 };
 
 struct MatchResult {
-  MatchResult(const std::string match, const size_t start, const size_t end,
-              const Sensitivity sensitivity = Sensitivity::Critical,
-              const double probability = 1.0) noexcept
-      : start_(start), end_(end), match_{match}, sensitivity_(sensitivity),
+  MatchResult(std::string match, size_t start, size_t end,
+              Sensitivity sensitivity = Sensitivity::Critical,
+              double probability = 1.0) noexcept
+      : start_(start), end_(end), match_(match), sensitivity_(sensitivity),
         probability_(probability){};
+  MatchResult(const MatchResult &other) noexcept
+    : start_(other.start_), end_(other.end_), match_(other.match_), sensitivity_(other.sensitivity_),
+        probability_(other.probability_){}
+  MatchResult& operator=(const MatchResult &other) noexcept {
+    if (this != &other) {
+      match_ = other.match_;
+      start_ = other.start_;
+      end_ = other.end_;
+      sensitivity_ = other.sensitivity_;
+      probability_ = other.probability_;
+    }
+
+    return *this;
+  }
+  bool operator==(const MatchResult &other) const noexcept = default;
+
+  [[nodiscard]] bool is_next_to(const MatchResult &other) const noexcept {
+    return (other.start_ - end_) == 2;
+  }
 
   [[nodiscard]] size_t start() const noexcept { return start_; }
 
@@ -37,11 +56,11 @@ struct MatchResult {
   [[nodiscard]] double probability() const noexcept { return probability_; }
 
 private:
-  const size_t start_;
-  const size_t end_;
-  const std::string match_;
-  const Sensitivity sensitivity_;
-  const double probability_;
+  size_t start_;
+  size_t end_;
+  std::string match_;
+  Sensitivity sensitivity_;
+  double probability_;
 };
 
 using MatchResults = std::vector<MatchResult>;
