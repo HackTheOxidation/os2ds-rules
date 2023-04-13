962e4679ba348fde72e77c6149a1ec4426680a52
HackTheOxidation <tomas.hagenau@protonmail.ch>
2023-04-13 09:52:22 +0200

    Add additional benchmark case for CPRRule.

--- tests/benchmarks/test_benchmark_cpr_detector.py
+++ tests/benchmarks/test_benchmark_cpr_detector.py
@@ -2,5 +2,9 @@ from os2ds_rules.cpr_detector import find_matches
 from .utilities import HTML_CONTENT
 
 
-def test_benchmark_cpr_detector(benchmark):
+def test_benchmark_cpr_detector_default(benchmark):
     matches = benchmark(find_matches, HTML_CONTENT)
+
+
+def test_benchmark_cpr_detector_check_mod11(benchmark):
+    matches = benchmark(find_matches, HTML_CONTENT, True)
