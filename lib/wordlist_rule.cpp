e739e22fcdc23cb5c36cc25790fb325e9863a63d
HackTheOxidation <tomas.hagenau@protonmail.ch>
2023-04-22 14:11:29 +0200

    Format new code for WordListRule.

--- lib/wordlist_rule.cpp
+++ lib/wordlist_rule.cpp
@@ -8,16 +8,21 @@ namespace OS2DSRules {
 
 namespace WordListRule {
 
-void WordListRule::check_match(MatchResults &results, const std::string candidate, const std::size_t start, const std::size_t stop) const noexcept {
+void WordListRule::check_match(MatchResults &results,
+                               const std::string candidate,
+                               const std::size_t start,
+                               const std::size_t stop) const noexcept {
   if (contains(candidate)) {
     results.push_back(MatchResult(candidate, start, stop));
   }
 }
 
-[[nodiscard]] MatchResults WordListRule::find_matches(const std::string &content) const noexcept {
+[[nodiscard]] MatchResults
+WordListRule::find_matches(const std::string &content) const noexcept {
   MatchResults results;
 
-  static const auto is_delimiter = make_predicate(' ', '\n', '.', ',', '\t', '!', '?');
+  static const auto is_delimiter =
+      make_predicate(' ', '\n', '.', ',', '\t', '!', '?');
 
   std::size_t start = 0;
   for (std::size_t i = 0; i < content.size(); ++i) {
@@ -32,17 +37,19 @@ void WordListRule::check_match(MatchResults &results, const std::string candidat
   return results;
 }
 
-
-[[nodiscard]] bool WordListRule::contains(const std::string_view target) const noexcept {
+[[nodiscard]] bool
+WordListRule::contains(const std::string_view target) const noexcept {
   return words_.contains(target);
 }
 
-[[nodiscard]] bool WordListRule::contains(const std::string target) const noexcept {
+[[nodiscard]] bool
+WordListRule::contains(const std::string target) const noexcept {
   return contains(std::string_view(target.cbegin(), target.cend()));
 }
 
-[[nodiscard]] bool WordListRule::contains(const std::string::const_iterator start,
-					  const std::string::const_iterator stop) const noexcept {
+[[nodiscard]] bool
+WordListRule::contains(const std::string::const_iterator start,
+                       const std::string::const_iterator stop) const noexcept {
   return contains(std::string_view(start, stop));
 }
 
